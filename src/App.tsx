f822f327fe4a784fc295bc859316605c5ff42b17
lewis <lewis@osas.solutions>
2025-09-13 03:36:21 +0100

    Yep it loads

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState, type ReactElement } from "react";
 import Artist from "./Artist"
 import type Song from "./SongInterface"
 
 function App() {
 
-  let lol2: Song[] = [];
+  let [artists, setArtists] = useState<ReactElement[]>();
 
-  // useEffect(() => {
+  // let artists: ReactElement[] = [];
 
-  // });
+  useEffect(() => {
+    fetch("data/data.json").then(async (resp) => {
+      let lol: ReactElement[] = [];
+
+      const data = await resp.json();
+
+      for(const lol2 of data) {
+        // console.log(lol2);
+        lol.push(<Artist username={lol2.username} displayname={lol2.displayname} tunes={[]}></Artist>)
+      }
+
+      setArtists(lol);
+      // artists.push(<h1>Yo</h1>)
+    });
+  }, []);
 
   return (
     <>
       <h1>EEEEVILLL GANGGGGG ARRRCHIVVVESS</h1>
-      <Artist username="usaname" displayname="displ" tunes={lol2}></Artist>
-      <Artist username="usanam2" displayname="anyame" tunes={lol2}></Artist>
+      <hr></hr>
+      {artists || <h1>loading boss please wait (probably broken idk)</h1>}
     </>
   )
 }
